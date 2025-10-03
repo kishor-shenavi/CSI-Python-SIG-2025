8945c87c66843b4c897494f0e9e3da9816eccc60
pushkarpawar01 <pawarpushkarprakash123@gmail.com>
2025-10-03 21:10:09 +0530

    added day 3 ppts and assignments

---
+++ DAY 3/codes/for-loop.py
@@ -0,0 +1,47 @@
+# n = 4
+# for i in range(0, n):
+#     print(i, end=' ')
+
+
+# s = ["Python", "Programming"]
+
+# for i in s:
+#     print(i)
+    
+# for i in range(5, 0, -1):
+#     print(i)
+
+# for i in range(1, 4):
+#     print(i)
+#     # if i == 2:
+#     #     break;
+# else:
+#     print("No Break\n")
+
+# numbers = [10, 21, 4, 45, 66, 93, 1]
+# even_count = 0
+# odd_count = 0
+# for num in numbers:
+#     if num % 2 == 0:
+#         even_count += 1
+#     else:
+#         odd_count += 1
+# print(f"Even numbers: {even_count}")
+# print(f"Odd numbers: {odd_count}")
+# name="john"
+# age=21
+# print("my name is:", name)
+# print(f"my name is: {name} and age is :{age}")
+# s = ["Python"]
+
+# for i in s:
+#     for j in i:
+#         print(j)
+
+
+rows = 5
+for i in range(1, rows + 1):       
+    for j in range(1, i + 1):      
+        print("*", end=' ')
+    print()
+
---
+++ DAY 3/codes/while-loop.py
@@ -0,0 +1,52 @@
+# i = 0
+# while i < 5:
+#     print("hello")
+#     i += 1
+
+# num = 1234
+# sum = 0
+# while num > 0:
+#     digit = num % 10
+#     sum += digit
+#     num //= 10
+
+# print("Sum of digits:", sum)
+
+
+num = 2
+i = 1
+while i <= 10:
+    print(f"{num} x {i} = {num * i}")
+    i += 1
+
+
+# num = 12345
+# rev = 0
+# while num > 0:
+#     digit = num % 10
+#     rev = rev * 10 + digit
+#     num //= 10
+
+# print("Reversed number:", rev)
+
+# i = 1  
+# while i <= 3:
+#     j = 1  
+#     while j <= 3:
+#         print("*", end=' ')
+#         j += 1
+#     print()  
+#     i += 1
+
+
+
+# for letter in 'python':
+#     if letter == 'h':
+#             continue
+#     print('Current Letter :', letter)
+
+
+# for letter in 'python':
+#     if letter == 't' :
+#         break
+#     print('Current Letter :', letter)
